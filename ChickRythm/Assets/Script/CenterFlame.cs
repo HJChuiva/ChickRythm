a633bf87ea5441eccd80b830e51b06569c46ada5
HJChuiva <95302018+HJChuiva@users.noreply.github.com>
2022-06-13 02:12:22 +0900

    1
    1

---
+++ ChickRythm/Assets/Script/CenterFlame.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CenterFlame : MonoBehaviour
+{
+    AudioSource myAudio;
+    bool musicStart = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAudio = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(!musicStart)
+        {
+            myAudio.Play();
+            musicStart = true;
+        }
+    }
+}
---
+++ ChickRythm/Assets/Script/Manager/NoteManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteManager : MonoBehaviour
+{
+    public int bpm = 0;
+    double currentTime = 0d;
+
+    [SerializeField] Transform tfNoteAppear = null;
+    [SerializeField] GameObject goNote = null;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime += Time.deltaTime;
+
+        if(currentTime >= 60d/bpm)
+        {
+            // 60/120 = 1beat 당 0.5초
+            //60s / bpm = 1beat 시간
+
+            GameObject t_note = Instantiate(goNote, tfNoteAppear.position, Quaternion.identity);
+            t_note.transform.SetParent(this.transform);
+            currentTime -= 60d / bpm;
+
+            //currentTime =0.51005..같은 오차가 손실되므로 currentTime은 0이 되면 안됨, 오차까지 생각
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Note"))
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
